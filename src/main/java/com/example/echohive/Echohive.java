57f672211648a4930f7e94e82bbba1d97dbbaf46
SantiaGhou <filipecacule@gmail.com>
2024-04-20 15:34:32 -0300

    mudança certa

--- src/main/java/com/example/echohive/Echohive.java
+++ src/main/java/com/example/echohive/Echohive.java
@@ -4,19 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class Echohive extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Echohive.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("EchoHive");
+        FXMLLoader fxmlLoader = new FXMLLoader(Echohive.class.getResource("register.fxml"));
+        stage.setTitle("EchoHive 0.0.1");
         stage.setScene(scene);
-        stage.setResizable(false);
-        stage.setWidth(720);
-        stage.setHeight(512);
+        stage.setResizable(false);   Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
     }
 
